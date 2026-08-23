43cb9aa8cf921a262e326fd567b27471025df290
Diego Oller de Oliveira <diegoollerdeoliveira@gmail.com>
2026-08-23 08:31:45 -0300

    add: Adicionadas rotas de get para Cursos e Turmas

---
+++ backend/rotas/university_factors.py
@@ -0,0 +1,54 @@
+import os
+
+import aiosqlite
+from fastapi import APIRouter, HTTPException, status, Path
+from pydantic import BaseModel, Field
+
+router = APIRouter(prefix="/fsa", tags=["Faculdade"])
+
+DB_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "database", "ecohora.db")
+
+
+class CoursesSchema(BaseModel):
+    id: int
+    nm_course: str
+
+@router.get("/get/courses", response_model=list[CoursesSchema], response_description="Retorna todos os cursos cadastrados.")
+async def get_courses():
+    """Retorna todos os cursos e suas IDs cadastrados no sistema."""
+
+    async with aiosqlite.connect(DB_PATH) as db:
+        db.row_factory = aiosqlite.Row
+        query = """
+        SELECT *
+        FROM courses
+        """
+
+        async with db.execute(query) as cur:
+            response = await cur.fetchall()
+            # O FastAPI ja identifica como lista de dicts para a documentação, mas naturalmente são apenas objetos, então ainda é feita a conversão.
+            response = [dict(row) for row in response]
+            return response 
+
+
+# Esse sistema é passível de mudança
+class ClassesScheme(BaseModel):
+    id: int
+    nm_class: str
+
+@router.get("/get/clases", response_model=list[ClassesScheme], response_description="Retorna as salas cadastradas.")
+async def get_classes():
+    """Retorna todas as turmas e suas IDs cadastradas no sistema."""
+
+    async with aiosqlite.connect(DB_PATH) as db:
+        db.row_factory = aiosqlite.Row
+        query = """
+        SELECT *
+        FROM classes
+        """
+
+        async with db.execute(query) as cur:
+            response = await cur.fetchall()
+            # O FastAPI ja identifica como lista de dicts para a documentação, mas naturalmente são apenas objetos, então ainda é feita a conversão.
+            response = [dict(row) for row in response]
+            return response 
