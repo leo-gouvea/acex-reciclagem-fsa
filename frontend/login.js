4869fe0ab68954316a00eb86dcc8ffbe6ec11160
Diego Oller de Oliveira <diegoollerdeoliveira@gmail.com>
2026-09-05 17:28:12 -0300

    fix: Corrigidas as rotas de auth e iniciadas as novas configs para homepage

--- frontend/login.js
+++ frontend/login.js
@@ -213,7 +213,7 @@ entrarForm.addEventListener("submit", async function (event) {
 
     // Redireciona para a página principal
     setTimeout(() => {
-      window.location.href = "index.html";
+      window.location.href = "menu.html";
     }, 800);
   } catch (erro) {
     console.error("Erro ao fazer login:", erro);
