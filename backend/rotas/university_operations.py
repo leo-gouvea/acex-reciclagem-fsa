593054e72e9a032342035d6010f5418016634e09
Diego Oller de Oliveira <diegoollerdeoliveira@gmail.com>
2026-09-10 04:44:47 -0300

    feat: Adiciona parcialmente as rotas de gerenciamento de cursos e turmas

--- backend/rotas/university_operations.py
+++ backend/rotas/university_operations.py
@@ -57,4 +57,48 @@ async def get_classes():
             response = await cur.fetchall()
             # O FastAPI ja identifica como lista de dicts para a documentação, mas naturalmente são apenas objetos, então ainda é feita a conversão.
             response = [{"id": row["id"], "class_code": row["ds_class_code"]} for row in response]
-            return response 
+            return response
+
+
+# ADICIONAR VERIFICAÇÃO DE HIERARQUIA POR COOKIE
+# Adiciona e remove um Curso ao DB
+class PostCourseRequest(BaseModel):
+    course_name: str = Field(..., min_length=5, examples=["Análise e Desenvolvimento de Sistemas"])
+
+class PostCourseReturn(BaseModel):
+    status: int
+    detail: str
+
+@router.post("/course", response_model=PostCourseReturn, response_description="Retorna se a inserção foi um sucesso")
+async def post_course(data: PostCourseRequest):
+    """
+    Adiciona um curso ao banco de Dados
+    Informe o nome do Curso a ser inserido
+    """
+    
+@router.delete("/course/{id}")
+async def delete_course(id: int):
+    """
+    Deleta um curso existente com base no ID
+    """
+
+# Adiciona e remove uma Turma ao DB
+class PostClassRequest(BaseModel):
+    class_code: str = Field(..., min_length=4, max_length=4, examples=["1A/M"])
+
+class PostClassReturn(BaseModel):
+    status: int
+    detail: str
+
+@router.post("/course", response_model=PostClassReturn, response_description="Retorna se a inserção foi um sucesso")
+async def post_class():
+    """
+    Adiciona uma turma ao banco de Dados
+    Informe a turma a ser inserida
+    """
+    
+@router.delete("/class/{id}", response_description="Retorna se a deleção foi um sucesso")
+async def delete_class(id: int):
+    """
+    Deleta um curso existente com base no ID
+    """
